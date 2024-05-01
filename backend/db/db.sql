17f4c85bc1a46fc84f3fd22d52f5bc584e75c913
amineaouini-notbot <aminaouini565@gmail.com>
2024-05-01 18:18:24 +0100

    type fix & admin

--- backend/db/db.sql
+++ backend/db/db.sql
@@ -1,11 +1,12 @@
-CREATE DATABASE auction_it
+CREATE DATABASE auction_it;
+USE auction_it;
 
 CREATE TABLE users (
     id serial PRIMARY KEY,
 	username VARCHAR ( 50 ) UNIQUE NOT NULL,
 	email VARCHAR ( 50 ) UNIQUE NOT NULL,
     user_pass VARCHAR ( 50 ) NOT NULL
-)
+);
 
 CREATE TABLE items (
     id serial PRIMARY KEY,
@@ -15,7 +16,7 @@ CREATE TABLE items (
     end_bids_at TIMESTAMP,
     created_at TIMESTAMP
     
-)
+);
 
 CREATE TABLE bids (
     id serial PRIMARY KEY,
@@ -23,13 +24,21 @@ CREATE TABLE bids (
 	item_id INT REFERENCES items(id),
     price DECIMAL(10, 2),
     created_at TIMESTAMP,
-)
+);
 
 
 CREATE TABLE comments (
     id serial PRIMARY KEY,
 	user_id INT REFERENCES users(id),
-	item_id INT REFERENCES item(id),
+	item_id INT REFERENCES items(id),
 	comment VARCHAR ( 101 ),
     created_at TIMESTAMP,
-)
+);
+
+CREATE TABLE _admin (
+    id serial PRIMARY KEY,
+    username VARCHAR ( 101 ) UNIQUE NOT NULL,
+    pass VARCHAR (101) UNIQUE NOT NULL
+);
+
+INSERT INTO _admin (username, pass) VALUES ('admin', 'admin');
