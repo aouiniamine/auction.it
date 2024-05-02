be677fc621635366aa34b131a6e1427cb688393c
amineaouini-notbot <aminaouini565@gmail.com>
2024-05-02 13:08:44 +0100

    init nav

--- client/app/page.js
+++ client/app/page.js
@@ -2,9 +2,15 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-    
-    
+    <main className="flex-box min-h-screen flex-col">
+      <nav className="flex py-6 px-20 justify-between">
+        <ul className="flex gap-20">
+          <li className="text-2xl">Auction</li>
+          <li className="text-2xl">In Auction Now</li>
+          <li className="text-2xl">About</li>
+        </ul>
+        <a className="self-end text-2xl">Log In</a>
+      </nav>
     </main>
   );
 }
