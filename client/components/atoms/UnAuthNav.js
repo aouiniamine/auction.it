6ded63270a41393bf9182723d2043992ef5ca4d9
amineaouini-notbot <aminaouini565@gmail.com>
2024-05-04 10:55:02 +0100

    fix top padding & set shadow style to nav

--- client/components/atoms/UnAuthNav.js
+++ client/components/atoms/UnAuthNav.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function UnAuthNav() {
   return (
-    <nav className="flex py-6 px-20 justify-between">
+    <nav className="flex py-3 px-20 justify-between shadow-sm bg-slate-100">
         <ul className="flex gap-20">
             <li className="text-2xl">Auction</li>
             <li className="text-2xl">In Auction Now</li>
