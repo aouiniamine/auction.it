5bd08deb7d7731b219c2c8cfa26f9e904321d604
amineaouini-notbot <aminaouini565@gmail.com>
2024-05-05 18:34:47 +0100

    finished style for images to upload

--- client/app/product/create/page.js
+++ client/app/product/create/page.js
@@ -1,7 +1,10 @@
 'use client'
 import ContentWrapper from '@/src/components/atoms/ContentWrapper'
 import Nav from '@/src/components/atoms/Nav'
+import DisplayImages from '@/src/components/atoms/images/DisplayImages'
+import UploadImage from '@/src/components/atoms/images/UploadImage'
 import { getAllCategories } from '@/src/utils/categoriesAPIs'
+import { Fleur_De_Leah } from 'next/font/google'
 import React, { useEffect, useRef, useState } from 'react'
 
 export default function page() {
@@ -11,14 +14,21 @@ export default function page() {
   const [startingPrice, setStartingPrice] = useState(null)
   const [endBidsAt, setEndBidsAt] = useState(null)
   const [categoriesList, setCategoriesList] = useState([])
-  const optionsRef = useRef()
+  const [images, setImages] = useState([])
+
+  const selectImages = (e) => {
+    const files = e.target.files
+    if(images.length + files.length <= 10){
+      setImages(files)
+
+    }
+  }
   useEffect(()=>{
     const fetchData = async () =>{
       try{
-
         const allCategories = await getAllCategories()
-        console.log(allCategories.data.categories)
         setCategoriesList(allCategories.data.categories)
+
       }catch(err){ console.log(err) }
     }
     fetchData()
@@ -29,14 +39,15 @@ export default function page() {
         <Nav/>
         <ContentWrapper>
           <form className='flex flex-col gap-7 items-center h-screen'>
-            <div>
-              <input type='file' accept="image/png, image/jpeg, image/webp" placeholder='you need to add images'/>
-
-            </div>
+            {!images.length ? 
+            <UploadImage onSelect={selectImages} />: (
+              <DisplayImages selectedImages={images} />
+            )}
+             
             <div className='flex flex-row gap-20'>
             <input type='text' onChange={(e)=>setTitle(e.target.value)} placeholder="write the product's title" className='bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-80 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500' />
             <input type='number' onChange={(e)=>setStartingPrice(e.target.value)} min={1} placeholder='starting price?' className='bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-80 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500' />
-            <select ref={optionsRef} onChange={e => setCategoryId(e.target.value)} className='bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-80 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500'>
+            <select onChange={e => setCategoryId(e.target.value)} className='bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-80 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500'>
               <option disabled selected>select a category</option>
               {categoriesList.map((category, i) =>(
               <option key={i} value={category.id}>{category.name}</option>
