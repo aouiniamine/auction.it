71fd38e3d99300448f0323dc2c0888ccb92e0359
amineaouini-notbot <aminaouini565@gmail.com>
2024-05-08 23:32:18 +0100

    fix image to max width

--- client/src/components/atoms/images/Image.js
+++ client/src/components/atoms/images/Image.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function Image({imageSource}) {
   return (
-    <div className='bg-gray-50 dark:bg-gray-600 rounded-xl p-4 mb-2 hover:bg-gray-200 dark:hover:bg-gray-500'>
+    <div className='bg-gray-50 dark:bg-gray-600 rounded-xl p-4 mb-2 hover:bg-gray-200 dark:hover:bg-gray-500 max-w-60'>
         <img src={imageSource} className='object-scale-down h-20 w-52'/>
     </div>
   )
